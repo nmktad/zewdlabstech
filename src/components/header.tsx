cefa1e72e110b2928c9feb1991a84c2e188a205e
nmktad <nmktadesse@gmail.com>
2025-02-10 22:05:58 +0300

    feat: add footer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,17 @@
 export default function Header() {
-  return <header>Header here</header>;
+  return (
+    <header>
+      {/* Logo */}
+      <div></div>
+
+      {/* Links and CTA components */}
+      <div>
+        {/* Links */}
+        <ul></ul>
+
+        {/* CTA components */}
+        <div></div>
+      </div>
+    </header>
+  );
 }
