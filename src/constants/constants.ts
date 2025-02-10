94ca663d69fe2e4af6d778f42b8dd1fa2c7416d8
nmktad <nmktadesse@gmail.com>
2025-02-10 15:58:04 +0300

    feat: update to new versions

--- src/constants/constants.ts
+++
@@ -1,9 +0,0 @@
-import { env } from "@/env/client";
-
-export const BASE_URL = env.NEXT_PUBLIC_URL ?? "http://localhost:3000";
-export const SUPPORT_EMAIL =
-  env.NEXT_PUBLIC_SUPPORT_EMAIL ?? "nmktadesse@gmail.com";
-
-export const SUBSCRIBE_URL = "https://subscription.dummy.com";
-
-export const IS_PRODUCTION = process.env.NODE_ENV === "production";
