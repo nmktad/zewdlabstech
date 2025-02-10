8a67c4b313e9d606517118a0053473d33199d3cd
nmktad <nmktadesse@gmail.com>
2025-02-10 23:42:32 +0300

    feat: remove stuff

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,4 @@
 import type { PropsWithChildren } from "react";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
 import Link from "next/link";
 
 export default function PublicLayout({
