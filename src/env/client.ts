479e72eb57e8ee46c34f103f1ff6a0b7e22ebac0
nmktad <nmktadesse@gmail.com>
2025-02-10 15:25:46 +0300

    feat: set optional env variables

--- src/env/client.ts
+++ src/env/client.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 
 export const env = createEnv({
   client: {
-    NEXT_PUBLIC_URL: z.string(),
-    NEXT_PUBLIC_SENTRY_DSN: z.string(),
+    NEXT_PUBLIC_URL: z.string().url().default("http://localhost:3000"),
+    NEXT_PUBLIC_SENTRY_DSN: z.string().optional(),
     NEXT_PUBLIC_SUPPORT_EMAIL: z.string().email().optional(),
   },
   runtimeEnv: {
