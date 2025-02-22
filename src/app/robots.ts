eb00a21c4584be9e9262bf6ee33f41f743a0714e
nmktad <nmktadesse@gmail.com>
2025-02-22 09:51:40 +0300

    add robots, manifest file

---
+++ src/app/robots.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from "next";
+
+// This allows us to generate a `robots.txt` file dynamically based on the needs of the Node.js Website
+// @see https://nextjs.org/docs/app/api-reference/file-conventions/metadata/robots
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/_next/", "/api/"],
+    },
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+  };
+}
+
+// Enforces that this route is used as static rendering
+// @see https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
+export const dynamic = "error";
